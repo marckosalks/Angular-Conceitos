14d23603068d28668bbd6954e74404cd9175a540
marckosalks <m4rcos.sales@gmail.com>
2025-06-29 02:29:27 -0300

    feat: ngSwitch

--- src/app/components/ng-switch/ng-switch.component.ts
+++ src/app/components/ng-switch/ng-switch.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-switch',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-switch.component.html',
   styleUrl: './ng-switch.component.scss'
 })
 export class NgSwitchComponent {
-
+  aba: string = 'home'
 }
